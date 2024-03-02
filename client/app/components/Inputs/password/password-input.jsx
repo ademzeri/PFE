8abcb11cb6a9497e096756958a65861c692c216c
Mohamed ncib <98429286+Mohamed-ncib19@users.noreply.github.com>
2024-03-02 01:14:58 +0100

    fix : resposive login page

--- client/app/components/Inputs/password/password-input.jsx
+++ client/app/components/Inputs/password/password-input.jsx
@@ -1,17 +1,37 @@
-import { IconEyeOff } from '@tabler/icons-react';
- import "./password-input.styles.css";
- const PasswordInput = () => {
-    return ( 
-        <div className="input-group col position-relative d-flex align-items-center">
-    <input type="password" className="form-control focus-blue-bottom-border rounded p-2" placeholder='password' />
-    <button className="input-group-button position-absolute end-0 z-index-999 border-none px-2 py-1 rounded" id='show-hide-password'>
-        <i><IconEyeOff /></i>
-    </button>
-</div>
+import { IconEyeOff, IconEye } from "@tabler/icons-react";
+import "./password-input.styles.css";
+import { useState } from "react";
 
-    
-    
-     );
-}
- 
+const PasswordInput = ({ register, errors }) => {
+  const [visible, setVisible] = useState(false);
+
+  const handleShowPassword = () => {
+    setVisible((visible) => !visible);
+  };
+
+  return (
+    <div>
+      <div className={`input-group position-relative d-flex align-items-center mb-3 ${errors.password ? 'is-invalid' : ''}`}>
+      <input
+  type={visible ? "text" : "password"}
+  className={`form-control focus-blue-bottom-border rounded p-2 ${errors.password ? "is-invalid" : ""}`}
+  placeholder="password"
+  name="password"
+  {...register}
+/>
+
+
+        <button
+          type="button"
+          className="input-group-button position-absolute end-0 z-index-999 border-none px-2 py-1 mx-2 rounded show-hide-password"
+          onClick={handleShowPassword}
+        >
+        <i className={` ${errors.password? "p-3" : ""}`} >{visible ? <IconEye /> : <IconEyeOff />}</i>
+         </button>
+      </div>
+      {errors.password && <p className="invalid-feedback">{errors.password.message}</p>}
+    </div>
+  );
+};
+
 export default PasswordInput;
--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -1,23 +1,25 @@
 'use client'
 import Image from "next/image";
-import illustartionBackground from "../../public/3D-illustration-background.png";
-import Logo from "../../public/logo.svg";
+import illustartionBackground from "../../public/login-3D-illustration-background.png";
+import Logo from "../../public/Logo.svg";
 import "./login.styles.css";
-import LoginForm from "../components/loginForm/loginForm";
+import LoginForm from "../components/login-Form/loginForm";
 const Login = () => {
     return (
-        <section className="d-flex justify-content-center align-items-center vh-100 rounded-circle" >
-            <div className="w-75 h-75 row rounded">
-                <div className="col bg-soft-tertiary-color " id="login-area">
-                    <Image src={Logo} alt="Logo" width={500}  />
+        <section className="d-flex justify-content-center align-items-center vh-100 " >
+            <div className=" row col-md-12 col-lg-10 col-xs-12 col-sm-12 h-75 m-auto shadow rounded">
+                <div className="col-sm-12 col-md-12 col-xs-12 col-lg-6 col-xl bg-soft-tertiary-color rounded d-flex justify-content-start align-items-center flex-column " id="login-area">
+                    <div className=" row" >
+                        <Image src={Logo} alt="Logo" className="m-auto col-12 w-auto"  />
+                    </div>
                     <LoginForm />
                 </div>
-                <div className="col-md position-relative d-none d-lg-flex justify-content-center align-items-center" id="background">
+                <div className="col position-relative d-none d-lg-flex justify-content-end align-items-center login-section-background ">
                 <Image
                     className="position-absolute  "
                     src={illustartionBackground}
-                    width={500}
-                    height={300}
+                    width={300}
+                   
                     alt="Login Background Illustration"
                 />
             </div>
